8e17bedd586f1e76c66e41cf7c7e6c3e2179b43f
Peter Farley <far.peter1@gmail.com>
2025-03-12 20:14:27 -0500

    tools: Fix lboot_prepare.sh
    This resolves two issues:
     - When used with a real floppy device, it would complain that it did
       not exist as it was not a regular file
     - It would fail when attempting to create the LBOOT directory when it
       already exists, as `mmd` does not have an option to not fail when the
       target directory exists

--- tools/lboot_prepare.sh
+++ tools/lboot_prepare.sh
@@ -24,7 +24,7 @@ STAGE2_MAP=$LBOOT_DIR/build/stage2.map
 
 SECTOR_MAPPER=$LBOOT_DIR/tools/sector_mapper/sector_mapper
 
-if   [[ ! -f $FLOPPY ]]; then
+if   [[ ! -e $FLOPPY ]]; then
     echo "Floppy/image '$FLOPPY' does not exist!"
     exit 2
 elif [[ ! -f $STAGE1 ]]; then
@@ -39,13 +39,22 @@ elif [[ ! -f $SECTOR_MAPPER ]]; then
 fi
 
 # Stage 1 / bootsector
+echo "Writing bootsector"
 dd if=$STAGE1 of=$FLOPPY conv=notrunc iflag=count_bytes,skip_bytes oflag=seek_bytes count=11
 dd if=$STAGE1 of=$FLOPPY conv=notrunc iflag=count_bytes,skip_bytes oflag=seek_bytes skip=90 seek=90
 
 # Stage 2 + Map
+echo "Creating LBOOT directory"
+set +e # mmd returns error if the directory exists
 mmd   -D oO -i $FLOPPY             ::/LBOOT
+set -e
+
+echo "Copying stage 2 bootloader"
 mcopy -D oO -i $FLOPPY $STAGE2     ::/LBOOT/STAGE2.BIN
+echo "Generating stage 2 sector map"
 $SECTOR_MAPPER 1 $FLOPPY "LBOOT/STAGE2.BIN" $STAGE2_MAP
+echo "Copying stage 2 sector map"
 mcopy -D oO -i $FLOPPY $STAGE2_MAP ::/LBOOT/STAGE2.MAP
+echo "Setting pointer to stage 2 sector map"
 $SECTOR_MAPPER 2 $FLOPPY "LBOOT/STAGE2.MAP"
 
